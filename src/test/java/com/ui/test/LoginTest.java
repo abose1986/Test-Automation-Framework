0395ba04da08114646c838ad54fe185e33625aa5
Abhishek Bose <shanky123.bose@gmail.com>
2025-06-14 11:01:39 -0400

    First Commit - Reducing the login test

--- src/test/java/com/ui/test/LoginTest.java
+++ src/test/java/com/ui/test/LoginTest.java
@@ -1,15 +1,33 @@
 package com.ui.test;
 
 import static com.constants.Browser.*;
+
 import com.ui.pages.HomePage;
+import static org.testng.Assert.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class LoginTest {
 
-    public static void main(String[] args) {
+    HomePage homePage;
 
-        HomePage homePage = new HomePage(CHROME);
-        String userName = homePage.goToLoginInPage()
+    @BeforeMethod(description = "Load the Homepage of the website")
+    public void setUp(){
+        homePage = new HomePage(EDGE);
+    }
+
+    /*
+    Test Method !!!
+        1. Test Script should be small
+        2. Conditionals, Looping and try-catch statements are not allowed
+           Test scripts should only follow the test steps
+        3. Reduce the use of local variables
+        4. You should have at least one assertion
+     */
+    @Test(description = "Verifies if the valid user is able to login into the application", groups = {"e2e", "sanity"})
+    public void loginTest() {
+        assertEquals(homePage.goToLoginInPage()
                 .doLoginWith("payeba8662@baxima.com", "password")
-                .getUserName();
+                .getUserName(),"Ricky Ponting");
     }
 }
