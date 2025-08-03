005ac41b33f737d39e5ff4ab237c8dda876b918f
Abhishek Bose <shanky123.bose@gmail.com>
2025-08-02 20:15:28 -0400

    created ExtentReporterUtility

--- src/test/java/com/ui/listeners/TestListener.java
+++ src/test/java/com/ui/listeners/TestListener.java
@@ -1,9 +1,7 @@
 package com.ui.listeners;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import static com.utility.ExtentReporterUtility.*;
 import com.utility.LoggerUtility;
 import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
@@ -15,49 +13,45 @@ import java.util.Arrays;
 public class TestListener implements ITestListener {
 
     Logger logger = LoggerUtility.getLogger(this.getClass());
-    ExtentSparkReporter extentSparkReporter;
-    ExtentReports extentReports;
-    ExtentTest extentTest;
+
 
     @Override
     public void onTestStart(ITestResult result) {
         logger.info(result.getMethod().getMethodName());
         logger.info(result.getMethod().getDescription());
         logger.info(Arrays.toString(result.getMethod().getGroups()));
-        extentTest = extentReports.createTest(result.getMethod().getMethodName());
+        createExtentTest(result.getMethod().getMethodName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
         logger.info("{} PASSED", result.getMethod().getMethodName());
-        extentTest.log(Status.PASS, result.getMethod().getMethodName());
+        getExtent().log(Status.PASS, result.getMethod().getMethodName());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
         logger.error("{} FAILED", result.getMethod().getMethodName());
         logger.error(result.getThrowable().getMessage());
-        extentTest.log(Status.FAIL, result.getMethod().getMethodName());
+        getExtent().log(Status.FAIL, result.getMethod().getMethodName());
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
         logger.warn("{} SKIPPED", result.getMethod().getMethodName());
-        extentTest.log(Status.SKIP, result.getMethod().getMethodName());
+        getExtent().log(Status.SKIP, result.getMethod().getMethodName());
     }
 
     @Override
     public void onStart(ITestContext context) {
         logger.info("Test Suite Started");
-        extentSparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "//report.html");
-        extentReports = new ExtentReports();
-        extentReports.attachReporter(extentSparkReporter);
+        setUpSparkReporter("report.html");
 
     }
 
     @Override
     public void onFinish(ITestContext context) {
         logger.info("Test Suite Completed");
-        extentReports.flush();
+        flushReport();
     }
 }
